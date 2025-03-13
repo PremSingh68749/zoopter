227f489434c5bf1edfa3729f6d84ac641212c4e7
PremSingh68749 <prems.wappnet@gmail.com>
2025-03-13 09:59:22 +0530

    frontend vite app initalized with tailwind css

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const app = require('./app');
 const { initializeSocket } = require('./socket');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 const server = http.createServer(app);
 
 initializeSocket(server);
 
-server.listen(port, () => {
+server.listen(port,"0.0.0.0",() => {
     console.log(`Server is running on port ${port}`);
 });
